aa6bc00a71dba9299dac8abb3005f682217f0a72
Richyrick <rickdv@outlook.com>
2015-04-21 15:01:26 +0200

    Added actionbar to settingsActivity

--- app/src/main/java/com/android/rickapps/roosterwijzigingchecker/SettingsActivity.java
+++ app/src/main/java/com/android/rickapps/roosterwijzigingchecker/SettingsActivity.java
@@ -1,16 +1,23 @@
 package com.android.rickapps.roosterwijzigingchecker;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
-
+    //TODO: Text in Actionbar uitvogelen
     @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //ActionBar actionBar = getActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+
+        setContentView(R.layout.activity_settings);
+
         // Define the xml file used for preferences
         addPreferencesFromResource(R.xml.preferences);
     }
+
 }
