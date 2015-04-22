3a372d772967db553568e2e9a90916c54c5f5a20
Richyrick <rickdv@outlook.com>
2015-04-22 21:21:07 +0200

    Temporary current value displayed (only if edited)

--- app/src/main/java/com/android/rickapps/roosterwijzigingchecker/SettingsFragment.java
+++ app/src/main/java/com/android/rickapps/roosterwijzigingchecker/SettingsFragment.java
@@ -1,13 +1,31 @@
 package com.android.rickapps.roosterwijzigingchecker;
 
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Instellingen laden
-        addPreferencesFromResource(R.xml.preferences);}
+        addPreferencesFromResource(R.xml.preferences);
+        SharedPreferences sharedPref = getPreferenceScreen().getSharedPreferences();
+        sharedPref.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        //Veranderde pref vinden
+        Preference pref = findPreference(key);
+        //Huidige summary krijgen
+        String summaryStr = (String)pref.getSummary();
+        //Nieuwe data opvragen
+        String prefixStr = sharedPreferences.getString(key, "");
+        //Nieuwe data als summary neerzetten
+        pref.setSummary(summaryStr.concat(": [").concat(prefixStr).concat("]"));
+    }
 }
