1f2e794256c1ce85d952898c4a8aa06ea1bd58f5
Richyrick <rickdv@outlook.com>
2015-04-23 16:34:54 +0200

    Added code to make summary current value

---
+++ app/src/main/java/com/android/rickapps/roosterwijzigingchecker/SettingsFragment.java
@@ -0,0 +1,118 @@
+package com.android.rickapps.roosterwijzigingchecker;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+
+import java.util.Set;
+
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Instellingen laden
+        addPreferencesFromResource(R.xml.preferences);}
+    // Alle onderstaande code afkomstig van
+    // http://gmariotti.blogspot.nl/2013/02/preference-summary-or-secondary-text.html
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+
+        initSummary();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Unregister the listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+
+        //update summary
+        updatePrefsSummary(sharedPreferences, findPreference(key));
+    }
+    /**
+     * Update summary
+     */
+    protected void updatePrefsSummary(SharedPreferences sharedPreferences,
+                                      Preference pref) {
+
+        if (pref == null)
+            return;
+
+        if (pref instanceof ListPreference) {
+            // List Preference
+            ListPreference listPref = (ListPreference) pref;
+            listPref.setSummary(listPref.getEntry());
+
+        } else if (pref instanceof EditTextPreference) {
+            // EditPreference
+            EditTextPreference editTextPref = (EditTextPreference) pref;
+            editTextPref.setSummary(editTextPref.getText());
+
+        } else if (pref instanceof MultiSelectListPreference) {
+            // MultiSelectList Preference
+            MultiSelectListPreference mlistPref = (MultiSelectListPreference) pref;
+            String summaryMListPref = "";
+            String and = "";
+
+            // Retrieve values
+            Set<String> values = mlistPref.getValues();
+            for (String value : values) {
+                // For each value retrieve index
+                int index = mlistPref.findIndexOfValue(value);
+                // Retrieve entry from index
+                CharSequence mEntry = index >= 0
+                        && mlistPref.getEntries() != null ? mlistPref
+                        .getEntries()[index] : null;
+                if (mEntry != null) {
+                    // add summary
+                    summaryMListPref = summaryMListPref + and + mEntry;
+                    and = ";";
+                }
+            }
+            // set summary
+            mlistPref.setSummary(summaryMListPref);
+
+        }
+    }
+    /*
+	 * Init summary
+	 */
+    protected void initSummary() {
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+            initPrefsSummary(getPreferenceManager().getSharedPreferences(),
+                    getPreferenceScreen().getPreference(i));
+        }
+    }
+
+    /*
+     * Init single Preference
+     */
+    protected void initPrefsSummary(SharedPreferences sharedPreferences,
+                                    Preference p) {
+        if (p instanceof PreferenceCategory) {
+            PreferenceCategory pCat = (PreferenceCategory) p;
+            for (int i = 0; i < pCat.getPreferenceCount(); i++) {
+                initPrefsSummary(sharedPreferences, pCat.getPreference(i));
+            }
+        } else {
+            updatePrefsSummary(sharedPreferences, p);
+        }
+    }
+}
