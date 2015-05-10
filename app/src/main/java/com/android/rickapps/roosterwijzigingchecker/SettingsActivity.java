0c90a0c0922bb611bdff27de17fd09c7d068849a
Richyrick <rickdv@outlook.com>
2015-05-10 11:32:33 +0200

    Made ready for Play-Store Alpha/Beta

--- app/src/main/java/com/android/rickapps/roosterwijzigingchecker/SettingsActivity.java
+++ app/src/main/java/com/android/rickapps/roosterwijzigingchecker/SettingsActivity.java
@@ -1,15 +1,11 @@
 package com.android.rickapps.roosterwijzigingchecker;
 
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-public class SettingsActivity extends ActionBarActivity{
-    @SuppressWarnings("deprecation")
+public class SettingsActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
