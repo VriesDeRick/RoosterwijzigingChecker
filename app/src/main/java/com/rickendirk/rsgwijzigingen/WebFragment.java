14412c1d8cef9070dc1cc60541715c06f8635dd9
Richyrick <rickdv@outlook.com>
2015-07-05 21:05:22 +0200

    Reverted most animation work

--- app/src/main/java/com/rickendirk/rsgwijzigingen/WebFragment.java
+++ app/src/main/java/com/rickendirk/rsgwijzigingen/WebFragment.java
@@ -1,7 +1,6 @@
 package com.rickendirk.rsgwijzigingen;
 
 
-import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -21,7 +19,6 @@ public class WebFragment extends Fragment {
     private boolean isLoading = false;
     private boolean isFinished = false;
     boolean is1eKeerGenegeerd = false;
-    ProgressDialog progressDialog;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
@@ -30,29 +27,19 @@ public class WebFragment extends Fragment {
         if (savedInstanceState != null){
             is1eKeerGenegeerd = true;
         }
-        progressDialog = new ProgressDialog(getActivity());
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        progressDialog.setMax(100);
-        progressDialog.setTitle("Pagina wordt geladen");
-        progressDialog.setMessage("Even geduld alstublieft, de pagina wordt geladen");
-        progressDialog.setProgressNumberFormat(null);
-
-
 
         webView = (WebView) mainView.findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setUseWideViewPort(true);
-        webView.setVerticalScrollBarEnabled(false);
-        webView.setHorizontalScrollBarEnabled(false);
         webView.setWebViewClient(new WebViewClient() {
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 isLoading = true;
                 isFinished = false;
-                progressDialog.setProgress(0);
-                progressDialog.show();
             }
 
             @Override
@@ -60,18 +47,9 @@ public class WebFragment extends Fragment {
                 isFinished = true;
                 isLoading = false;
                 //Ondestaande if voorkomt weergeven "pagina is vernieuwd" bij oriëntatie-verandering
-                if (is1eKeerGenegeerd) {
+                if (is1eKeerGenegeerd){
                     is1eKeerGenegeerd = false;
-                } else
-                    Toast.makeText(getActivity(), "Pagina is vernieuwd", Toast.LENGTH_LONG).show();
-                progressDialog.dismiss();
-            }
-        });
-        webView.setWebChromeClient(new WebChromeClient(){
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                progressDialog.setProgress(newProgress);
-                super.onProgressChanged(view, newProgress);
+                } else Toast.makeText(getActivity(), "Pagina is vernieuwd", Toast.LENGTH_LONG).show();
             }
         });
 
