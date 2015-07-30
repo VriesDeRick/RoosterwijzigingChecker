6a215737e52db387c423102e6c7eecb7716acfee
Richyrick <rickdv@outlook.com>
2015-07-30 14:23:26 +0200

    Added timePickers, not checked for stability yet

--- app/src/main/java/com/rickendirk/rsgwijzigingen/SettingsFragment.java
+++ app/src/main/java/com/rickendirk/rsgwijzigingen/SettingsFragment.java
@@ -10,16 +10,33 @@ import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
+import com.android.datetimepicker.time.RadialPickerLayout;
+import com.android.datetimepicker.time.TimePickerDialog;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.Set;
 
 public class SettingsFragment extends PreferenceFragment implements
-        SharedPreferences.OnSharedPreferenceChangeListener {
+        SharedPreferences.OnSharedPreferenceChangeListener, TimePickerDialog.OnTimeSetListener {
+    private static final String TIME_PATTERN = "HH:mm";
+    private Calendar calendar1;
+    private Calendar calendar2;
+    private SimpleDateFormat timeFormat;
+    //int om bij te houden op welke v/d 2 zoektijden is gedrukt
+    int welke = 0;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setupCalendar();
+        timeFormat = new SimpleDateFormat(TIME_PATTERN, Locale.getDefault());
 
         // Instellingen laden
         addPreferencesFromResource(R.xml.preferences);
@@ -29,10 +46,10 @@ public class SettingsFragment extends PreferenceFragment implements
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 new AlertDialog.Builder(getActivity())
-                    .setTitle("Over Roosterwijzigingchecker")
-                    .setMessage(getString(R.string.overDezeApp))
-                    .setPositiveButton("OK", null)
-                    .show();
+                        .setTitle("Over Roosterwijzigingchecker")
+                        .setMessage(getString(R.string.overDezeApp))
+                        .setPositiveButton("OK", null)
+                        .show();
                 return true;
             }
         });
@@ -78,8 +95,72 @@ public class SettingsFragment extends PreferenceFragment implements
                 return true;
             }
         });
+        Preference time1 = findPreference("pref_auto_tijd1");
+        final TimePickerDialog.OnTimeSetListener listener = this;
+        time1.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                welke = 1;
+                TimePickerDialog.newInstance(listener, calendar1.get(Calendar.HOUR_OF_DAY),
+                        calendar1.get(Calendar.MINUTE), true).show(getFragmentManager(), "timePicker1");
+                return true;
+            }
+        });
+        Preference time2= findPreference("pref_auto_tijd2");
+        time2.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                welke = 2;
+                TimePickerDialog.newInstance(listener, calendar2.get(Calendar.HOUR_OF_DAY),
+                        calendar2.get(Calendar.MINUTE), true).show(getFragmentManager(), "timePicker2");
+                return true;
+            }
+        });
 
 
+    }
+
+    private void setupCalendar() {
+        calendar1 = Calendar.getInstance();
+        calendar2 = Calendar.getInstance();
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        long timeInMsCal1 = sp.getLong("timeMs1", 0);
+        long timeInMsCal2 = sp.getLong("timeMs2", 0);
+        //Als er geen tijd is huidige tijd pakken
+        if (timeInMsCal1 == 0){
+            timeInMsCal1 = Calendar.getInstance().getTimeInMillis();
+        }
+        if (timeInMsCal2 == 0){
+            timeInMsCal2 = Calendar.getInstance().getTimeInMillis();
+        }
+        calendar1.setTimeInMillis(timeInMsCal1);
+        calendar2.setTimeInMillis(timeInMsCal2);
+    }
+    @Override
+    public void onTimeSet(RadialPickerLayout radialPickerLayout, int hourOfDay, int minute) {
+        if (welke == 1){
+            calendar1.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            calendar1.set(Calendar.MINUTE, minute);
+            saveToSP(1);
+        } else{
+            calendar2.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            calendar2.set(Calendar.MINUTE, minute);
+            saveToSP(2);
+        }
+        //TODO: Verwijzing naar Dirks Method hier
+    }
+
+    private void saveToSP(int welke) {
+        SharedPreferences.Editor spEditor = PreferenceManager
+                .getDefaultSharedPreferences(getActivity()).edit();
+        if (welke == 1){
+            long timeMs1 = calendar1.getTimeInMillis();
+            spEditor.putLong("timeMs1", timeMs1);
+        } else{
+            long timeMs2 = calendar2.getTimeInMillis();
+            spEditor.putLong("timeMs2", timeMs2);
+        }
+        spEditor.commit();
     }
 
 
@@ -186,4 +267,6 @@ public class SettingsFragment extends PreferenceFragment implements
             updatePrefsSummary(sharedPreferences, p);
         }
     }
+
+
 }
