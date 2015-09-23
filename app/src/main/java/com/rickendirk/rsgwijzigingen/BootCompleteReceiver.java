5868cc0e20e920b16cd3b73b1159b16a42509b5a
Richyrick <rickdv@outlook.com>
2015-09-23 16:02:14 +0200

    Fixed bug where alarm would fire immediately after boot

--- app/src/main/java/com/rickendirk/rsgwijzigingen/BootCompleteReceiver.java
+++ app/src/main/java/com/rickendirk/rsgwijzigingen/BootCompleteReceiver.java
@@ -3,7 +3,10 @@ package com.rickendirk.rsgwijzigingen;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+
+import java.util.Calendar;
 
 public class BootCompleteReceiver extends BroadcastReceiver {
     @Override
@@ -11,8 +14,41 @@ public class BootCompleteReceiver extends BroadcastReceiver {
         Boolean isAlarmsEnabled = PreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean("pref_auto_zoek", false);
         if (isAlarmsEnabled) {
+            setupCalendarTimes(context);
             AlarmsSetter setter = new AlarmsSetter(context);
             setter.setupAlarms();
         }
+    }
+
+    private void setupCalendarTimes(Context context) {
+        Calendar calendar1 = Calendar.getInstance();
+        Calendar calendar2 = Calendar.getInstance();
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+
+        long timeInMsCal1 = sp.getLong("timeMs1", 0);
+        long timeInMsCal2 = sp.getLong("timeMs2", 0);
+
+        calendar1.setTimeInMillis(timeInMsCal1);
+        calendar2.setTimeInMillis(timeInMsCal2);
+
+        //Datum mag niet voor huidige datumn zijn, daat gaat hij direct
+        boolean isVeranderd = false;
+        if (calendar1.before(Calendar.getInstance())) {
+            calendar1.set(Calendar.DAY_OF_YEAR, Calendar.getInstance().get(Calendar.DAY_OF_YEAR));
+            isVeranderd = true;
+        }
+
+        if (calendar2.before(Calendar.getInstance())) {
+            calendar2.set(Calendar.DAY_OF_YEAR, Calendar.getInstance().get(Calendar.DAY_OF_YEAR));
+            isVeranderd = true;
+        }
+
+        if (isVeranderd){
+            SharedPreferences.Editor spEditor = sp.edit();
+            spEditor.putLong("timeMs1", calendar1.getTimeInMillis());
+            spEditor.putLong("timeMs2", calendar2.getTimeInMillis());
+            spEditor.commit();
+        }
+
     }
 }
